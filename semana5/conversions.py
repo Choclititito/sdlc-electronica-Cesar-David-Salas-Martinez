006f8d54df6971516ca3a2037ec690b68d1c345d
Choclititito <cesard9092005@gmail.com>
2026-08-15 07:53:09 -0600

    feat: add conversions module to semana5

---
+++ semana5/conversions.py
@@ -0,0 +1,10 @@
+def celsius_to_fahrenheit(c: float) -> float:
+    """Convert a Celsius temperature to Fahrenheit rounded to 2 decimals.
+
+    Args:
+        c: Temperature in degrees Celsius.
+
+    Returns:
+        Temperature in degrees Fahrenheit rounded to 2 decimal places.
+    """
+    return round(c * 9.0 / 5.0 + 32.0, 2)
